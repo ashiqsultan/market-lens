71b2a83d2d51a7aee56c5d16db7b4ff713a11466
ashiqsultan <ashiqdrive@gmail.com>
2024-05-09 12:13:09 +0530

    Added test fetch call

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('/hello');
+      const json = await response.json();
+      setData(json);
+    };
+    getData();
+  }, []);
+
   return (
     <>
       <p>This is Market Lens Frontend</p>
+      <p>{String(data)}</p>
     </>
   );
 }
