37264b2195f3711d083ab375fdd60ad6466b13ca
ashiqsultan <ashiqdrive@gmail.com>
2024-05-10 19:23:29 +0530

    Added financial APIs

---
+++ netlify/edge-functions/utils/getFinancial.ts
@@ -0,0 +1,22 @@
+import handlePolygonAPIRes from './handlePolygonAPIRes.ts';
+import { AppRes } from './types.ts';
+
+const POLYGON_API_KEY = Netlify.env.get('POLYGON_API_KEY');
+
+async function getFinancial(symbol: string): Promise<AppRes> {
+  try {
+    const polygonRes = await fetch(
+      `https://api.polygon.io/vX/reference/financials?ticker=${symbol}&timeframe=annual&limit=3&apiKey=${POLYGON_API_KEY}`
+    );
+    const response = await handlePolygonAPIRes(polygonRes);
+    return response;
+  } catch (error) {
+    console.error(error);
+    return {
+      status: 500,
+      data: JSON.stringify('Error in Logic or external API'),
+    };
+  }
+}
+
+export default getFinancial;
